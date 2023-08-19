4b2eabc0129750354d9963559d4e10bc2b9f9cc1
hamadioumar <hamadioumar00@gmail.com>
2023-08-19 12:27:51 +0000

    add new middleware

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const morgan = require("morgan");
+const helmet = require("helmet");
 const Joi = require("joi");
 const logger = require("./logger");
 const auth = require("./auth");
@@ -5,9 +7,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
 
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled...");
+}
 app.use(logger);
-
 app.use(auth);
 
 const genres = [
